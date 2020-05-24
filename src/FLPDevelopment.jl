1d871856cc7f89ce15679922bc9547397d0cb491
DarioSarra <dariosarra@gmail.com>
2020-05-24 13:31:54 +0100

    added deps for tests

--- src/FLPDevelopment.jl
+++ src/FLPDevelopment.jl
@@ -1,10 +1,14 @@
 module FLPDevelopment
 
 using Reexport
-@reexport using FLPprocess, DataFrames, Dates
+@reexport using FLPprocess, DataFrames, Dates, MixedModels, CSV
+import Distributions:Chisq
+
 
 include("groups.jl")
+include("DOFtests.jl")
 
 export youngs, age_exp, cnos_animals, wt_animals, below_thrs
+export Likelyhood_Ratio_test, AIC_test, AICc_test
 
 end # module
