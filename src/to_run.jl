e2483e1cc38130562997ee1d3d55449b87807868
DarioSarra <dariosarra@gmail.com>
2020-05-26 08:14:14 +0100

    Analysis done

--- src/to_run.jl
+++ src/to_run.jl
@@ -18,31 +18,31 @@ CSV.write("/home/beatriz/mainen.flipping.5ht@gmail.com/Flipping/run_task_2/Dev_r
 proj = filter(r -> r.Exp_type == "Projections",data)
 CSV.write("/home/beatriz/mainen.flipping.5ht@gmail.com/Flipping/run_task_2/Dev_raw_data/Results_2020-05-24/cno_streaks.csv",proj)
 ######### Analysis Projection Experiment #########
-replace!(proj.PreInterpoke, missing => 0.0)
-filt = filter(r -> r.Num_pokes > 1 &&
-    r.Streak > 10 &&
-    r.PreInterpoke <= 10 &&
-    !r.LowPerformance,
+#data filtering
+replace!(proj.PreInterpoke, missing => 0.0) # missing to 0 nothing to worry this data will excluded anyway beacuse it occurs only if they poke just one time
+filt = filter(r -> r.Num_pokes > 1 && # at least 2 pokes per trial
+    r.Streak > 10 && #exclude the first 10 trials when they have no idea what is going on
+    r.PreInterpoke <= 10 && # exclude streaks where the interpoke is longer than 10 seconds
+    !r.LowPerformance, # exclude animlas that did less than 15 trials in their session
     proj);
-###
+### this is for plotting the mean plus sem bar plot ###
 df1 = combine(:AfterLast => mean, groupby(filt,[:Combo, :MouseID]))
 df2 = combine(:AfterLast_mean => a -> (Mean = mean(a), SEM = sem(a)),groupby(df1,:Combo))
 @df df2 bar(:Combo,:Mean, yerror  = :SEM,
     yticks = 0:0.25:4,
     linecolor = :black, markerstrokecolor = :black)
 savefig("/home/beatriz/Documents/Dario_reports/development/mean_sem.pdf");
-##
+## this is to plot the boxplot ##
 @df filt boxplot(:Combo,:AfterLast)
 savefig("/home/beatriz/Documents/Dario_reports/development/boxplot.pdf");
-##
+## here is the analysis explained in the email
 hetcno = filter(r -> r.Combo == "HETCNO",filt).AfterLast;
 hetsal = filter(r -> r.Combo == "HETSAL",filt).AfterLast;
 wtcno = filter(r -> r.Combo == "WTCNO",filt).AfterLast;
 pvalue(KSampleADTest(hetcno,hetsal,wtcno))
 pvalue(MannWhitneyUTest(hetcno,hetsal))
 pvalue(MannWhitneyUTest(hetcno,wtcno))
-
-##
+## Alternative analysis
 pvalue(KruskalWallisTest(wtcno,hetsal,wtcno))
 pvalue(KruskalWallisTest(hetcno,hetsal))
 pvalue(KruskalWallisTest(wtcno,hetsal))
