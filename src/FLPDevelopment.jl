f36a247b87125a25720905312eaed8e7d82c0ff4
DarioSarra <dariosarra@gmail.com>
2020-07-21 20:48:13 +0100

    finalised analysis

--- src/FLPDevelopment.jl
+++ src/FLPDevelopment.jl
@@ -1,15 +1,16 @@
 module FLPDevelopment
 
 using Reexport
-@reexport using FLPprocess, DataFrames, Dates, GLM, CSV, StatsBase, StatsPlots, HypothesisTests
+@reexport using FLPprocess, DataFrames, Dates, GLM, MixedModels, CSV, StatsBase, StatsPlots, HypothesisTests
 import Distributions:Chisq
-
+import Statistics: median
 
 include("groups.jl")
 include("utilities.jl")
 include("DOFtests.jl")
 
 export youngs, age_exp, cnos_animals, wt_animals, below_thrs, VirusDict
-export Likelyhood_Ratio_test, AIC_test, AICc_test, summarize
+export Likelyhood_Ratio_test, AIC_test, AICc_test
+export median
 
 end # module
--- src/Figures.jl
+++ src/Figures.jl
@@ -2,27 +2,62 @@ using Revise, FLPDevelopment, BrowseTables
 gr(size=(600,600), tick_orientation = :out, grid = false,
     linecolor = :black,
     markerstrokecolor = :black,
-    thickness_scaling = 2)
+    thickness_scaling = 2,
+    markersize = 10)
+##
 include("Young_to_run.jl")
 include("Caspase_to_run.jl")
 for df in (Age_p, Age_b, Age_s, Cas_p, Cas_b, Cas_s)
     filter!(r -> r.Protocol == "90/90", df)
 end
+Cas_s[!,:Gen] = [g == "HET" ? "Rbp4-cre" : "Wild Type" for g in Cas_s.Gen]
+Cas_s[!,:Combo] = Cas_s.Gen .* "\n" .* Cas_s.Virus
+Cas_s[!,:Group] = [r.Gen == "Wild Type" ? "Wild Type" : r.Gen .* "\n" .* r.Virus for r in eachrow(Cas_s)]
+categorical!(Cas_s,[:Gen,:Virus,:Group])
+levels!(Cas_s.Gen,["Wild Type","Rbp4-cre",])
+levels!(Cas_s.Virus,["tdTomato", "Caspase"])
+levels!(Cas_s.Group,["Rbp4-cre\ntdTomato", "Rbp4-cre\nCaspase", "Wild Type",])
+##
 #=
  Young vs Adults mice are in the Age labelled dataset (Age_p: pokes, Age_b: bouts Age_s: streaks)
  Caspase vs tdTomato mice are in the Cas labelled dataset (Cas_p: pokes, Cas_b: bouts Cas_s: streaks)
 =#
-filter!(r->r.AfterLast <= quantile(Age_s.AfterLast,0.95),Age_s)
-AfterLast_young = filter(r -> r.Gen == "Young",Age_s).AfterLast
-AfterLast_adult = filter(r -> r.Gen == "Adult",Age_s).AfterLast
-ApproximateTwoSampleKSTest(AfterLast_young,AfterLast_adult)
-res = summarize(Age_s,:Gen,:AfterLast)
-@df res scatter([1,2],:Mean, yerror = :SEM,
-    xlims = (0.5,2.5),
-    xticks = (1:2,:Xaxis))
-
-
-filter!(r->r.AfterLast <= quantile(Cas_s.AfterLast,0.95),Cas_s)
-AfterLast_caspase = filter(r -> r.Virus == "Caspase",Cas_s).AfterLast
-AfterLast_tdtomato = filter(r -> r.Virus == "tdTomato",Cas_s).AfterLast
-ApproximateTwoSampleKSTest(AfterLast_caspase,AfterLast_tdtomato)
+##
+cas_df = filter(r->
+    r.Gen == "Rbp4-cre" &&
+    r.ProtocolSession == 1  &&
+    r.AfterLast <= quantile(Cas_s.AfterLast,0.95),Cas_s)
+gd1 = groupby(cas_df,[:Gen,:Virus,:MouseID])
+c1 = combine(:AfterLast => mean => :AfterLast,gd1)
+c1[!,:xpos] = [v == "tdTomato" ? 1 : 2  for v in c1.Virus]
+c2 = combine(groupby(c1,[:Gen, :Virus])) do df
+    (Mean = mean(df.AfterLast), SEM = sem(df.AfterLast))
+end
+@df c2 scatter(1:nrow(c2),:Mean, yerror = :SEM,
+    xlims = (0.5, nrow(c2) + 0.5),
+    xticks = (1:nrow(c2),:Virus),
+    legend = false)
+@df c1 scatter!(:xpos,:AfterLast, markersize = 3, alpha = 0.5, color = :grey)
+c1[1,:Gen] == "Wild Type"
+crecas = filter(r -> r.Virus == "Caspase",c1).AfterLast
+cretd = filter(r -> r.Virus == "tdTomato",c1).AfterLast
+pvalue(EqualVarianceTTest(crecas,cretd))
+##
+age_df = filter(r->
+    r.ProtocolSession == 1 &&
+    r.AfterLast <= quantile(Cas_s.AfterLast,0.95),
+    Age_s)
+gd1 = groupby(age_df,[:Gen,:MouseID])
+c1 = combine(:AfterLast => mean => :AfterLast,gd1)
+c1[!,:xpos] = [g == "Adult" ? 2 : 1  for g in c1.Gen]
+c2 = combine(groupby(c1,[:Gen])) do df
+    (Mean = mean(df.AfterLast), SEM = sem(df.AfterLast))
+end
+@df c2 scatter(1:nrow(c2),:Mean, yerror = :SEM,
+    xlims = (0.5, nrow(c2) + 0.5),
+    xticks = (1:nrow(c2),:Gen),
+    legend = false)
+@df c1 scatter!(:xpos,:AfterLast, markersize = 3, alpha = 0.5, color = :grey)
+young = filter(r -> r.Gen == "Young",c1).AfterLast
+adult = filter(r -> r.Gen == "Adult",c1).AfterLast
+pvalue(EqualVarianceTTest(young,adult))
