f3fbd645d6e6012621fddf393ac71e40d6351ebb
DarioSarra <dariosarra@gmail.com>
2020-07-22 13:22:20 +0100

    adjusted confidence intervals

--- src/FLPDevelopment.jl
+++ src/FLPDevelopment.jl
@@ -1,12 +1,12 @@
 module FLPDevelopment
 
 using Reexport
-@reexport using FLPprocess, DataFrames, Dates, GLM, MixedModels, CSV, StatsBase, StatsPlots, HypothesisTests
+@reexport using FLPprocess, DataFrames, Dates, GLM, MixedModels, CSV, StatsBase, StatsPlots, HypothesisTests, Bootstrap
 import Distributions:Chisq
 import Statistics: median
 
 include("groups.jl")
-include("utilities.jl")
+# include("utilities.jl")
 include("DOFtests.jl")
 
 export youngs, age_exp, cnos_animals, wt_animals, below_thrs, VirusDict
--- src/Figures.jl
+++ src/Figures.jl
@@ -3,7 +3,7 @@ gr(size=(600,600), tick_orientation = :out, grid = false,
     linecolor = :black,
     markerstrokecolor = :black,
     thickness_scaling = 2,
-    markersize = 10)
+    markersize = 8)
 ##
 include("Young_to_run.jl")
 include("Caspase_to_run.jl")
@@ -26,34 +26,36 @@ levels!(Cas_s.Group,["Rbp4-cre\ntdTomato", "Rbp4-cre\nCaspase", "Wild Type",])
 cas_df = filter(r->
     r.Gen == "Rbp4-cre" &&
     r.ProtocolSession == 1  &&
-    r.AfterLast <= quantile(Cas_s.AfterLast,0.95),Cas_s)
-gd1 = groupby(cas_df,[:Gen,:Virus,:MouseID])
+    r.AfterLast <= quantile(Cas_s.AfterLast,0.90),Cas_s)
+gd1 = groupby(cas_df,[:Virus,:MouseID])
 c1 = combine(:AfterLast => mean => :AfterLast,gd1)
 c1[!,:xpos] = [v == "tdTomato" ? 1 : 2  for v in c1.Virus]
-c2 = combine(groupby(c1,[:Gen, :Virus])) do df
-    (Mean = mean(df.AfterLast), SEM = sem(df.AfterLast))
+c2 = combine(groupby(c1,:Virus)) do df
+    (Mean = mean(df.AfterLast), ERR = confint(OneSampleTTest(df.AfterLast))[2] - mean(df.AfterLast))
 end
-@df c2 scatter(1:nrow(c2),:Mean, yerror = :SEM,
+@df c2 scatter(1:nrow(c2),:Mean, yerror = :ERR,
     xlims = (0.5, nrow(c2) + 0.5),
     xticks = (1:nrow(c2),:Virus),
     legend = false)
 @df c1 scatter!(:xpos,:AfterLast, markersize = 3, alpha = 0.5, color = :grey)
-c1[1,:Gen] == "Wild Type"
 crecas = filter(r -> r.Virus == "Caspase",c1).AfterLast
 cretd = filter(r -> r.Virus == "tdTomato",c1).AfterLast
 pvalue(EqualVarianceTTest(crecas,cretd))
+pvalue(MannWhitneyUTest(crecas,cretd))
+pvalue(ApproximateTwoSampleKSTest(crecas,cretd))
 ##
 age_df = filter(r->
     r.ProtocolSession == 1 &&
-    r.AfterLast <= quantile(Cas_s.AfterLast,0.95),
+    r.AfterLast <= quantile(Cas_s.AfterLast,0.90),
     Age_s)
 gd1 = groupby(age_df,[:Gen,:MouseID])
 c1 = combine(:AfterLast => mean => :AfterLast,gd1)
 c1[!,:xpos] = [g == "Adult" ? 2 : 1  for g in c1.Gen]
 c2 = combine(groupby(c1,[:Gen])) do df
-    (Mean = mean(df.AfterLast), SEM = sem(df.AfterLast))
+    (Mean = mean(df.AfterLast), ERR = confint(OneSampleTTest(df.AfterLast))[2] - mean(df.AfterLast))
 end
-@df c2 scatter(1:nrow(c2),:Mean, yerror = :SEM,
+
+@df c2 scatter(1:nrow(c2),:Mean, yerror = :ERR,
     xlims = (0.5, nrow(c2) + 0.5),
     xticks = (1:nrow(c2),:Gen),
     legend = false)
@@ -61,3 +63,5 @@ end
 young = filter(r -> r.Gen == "Young",c1).AfterLast
 adult = filter(r -> r.Gen == "Adult",c1).AfterLast
 pvalue(EqualVarianceTTest(young,adult))
+pvalue(MannWhitneyUTest(young,adult))
+pvalue(ApproximateTwoSampleKSTest(young,adult))
