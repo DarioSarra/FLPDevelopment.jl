f31ee0af7227cfd1764d6bc8968725a955c3a5b1
DarioSarra <dariosarra@gmail.com>
2020-09-26 11:52:10 +0100

    new figures

--- src/FLPDevelopment.jl
+++ src/FLPDevelopment.jl
@@ -16,7 +16,7 @@ include("Mixture_fit.jl")
 
 export youngs,dario_youngs, females, age_exp, cnos_animals, wt_animals, below_thrs, VirusDict
 export Likelyhood_Ratio_test, AIC_test, AICc_test
-export median, std, kde, dvAnalysis
+export median, std, kde, dvAnalysis, DoubleAnalysis
 export fraction_true, bootstrap_mean, frequency
 export mouse_summary,  group_summary, dvplot
 export poke_plot!
