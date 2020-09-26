fea73d01c860efcaf52e416bc985862e6b975ac8
DarioSarra <dariosarra@gmail.com>
2020-09-26 11:51:47 +0100

    Perform both parametric and non parametric analysis

--- src/Analysis_type.jl
+++ src/Analysis_type.jl
@@ -21,10 +21,49 @@ function dvAnalysis(df,xvar,yvar; yspan = :auto, ystep = :auto, bs = false, nonp
     df1 = mouse_summary(df,xvar,yvar; summary = summary)
     normality = test_normality(df1,xvar,yvar)
     if nonparametric
-        normality = false
+        norm = false
+    else
+
     end
     df2 = group_summary(df1,xvar,yvar; normality = normality)
-    test = test_difference(df1,xvar,yvar)
+    test = test_difference(df1,xvar,yvar; normality = normality)
     plt = dvplot(df1,df2,xvar,yvar,test; yspan = yspan, ystep = ystep)
     dvAnalysis(df1, df2, normality, test, plt)
+end
+
+struct DoubleAnalysis
+    mice_summary
+    JarqueBera
+    parametric_summary
+    UnequalVarianceT
+    parametric_plot
+    nonparametric_summary
+    MannWhitneyU
+    nonparametric_plot
+end
+
+function DoubleAnalysis(df,xvar,yvar; yspan = :auto, ystep = :auto, bs = false)
+    if eltype(df[:,yvar]) == Bool
+        println("Boolean vector, using fraction as summary")
+        summary = fraction_true
+    else
+        if bs
+            println("using bootstrap_mean as mouse summary")
+            summary = bootstrap_mean
+        else
+            summary = mean
+        end
+    end
+    df1 = mouse_summary(df,xvar,yvar; summary = summary)
+    JarqueBera = test_normality(df1,xvar,yvar)
+    parametric_summary = group_summary(df1,xvar,yvar; normality = true)
+    UnequalVarianceT = test_difference(df1,xvar,yvar; normality = true)
+    parametric_plot = dvplot(df1,parametric_summary,xvar,yvar,UnequalVarianceT; yspan = yspan, ystep = ystep)
+    nonparametric_summary = group_summary(df1,xvar,yvar; normality = false)
+    MannWhitneyU = test_difference(df1,xvar,yvar; normality = false)
+    nonparametric_plot = dvplot(df1,nonparametric_summary,xvar,yvar,MannWhitneyU; yspan = yspan, ystep = ystep)
+
+    DoubleAnalysis(df1, JarqueBera,
+        parametric_summary, UnequalVarianceT, parametric_plot,
+        nonparametric_summary, MannWhitneyU, nonparametric_plot)
 end
