a7124485e0c1d61a94f2910a34b067814d049373
DarioSarra <dariosarra@gmail.com>
2020-09-28 16:28:25 +0100

    update groups

--- src/groups.jl
+++ src/groups.jl
@@ -23,11 +23,14 @@ const age_exp = vcat(Nbs,Bns,Its)
 # specifies which animals in age_exp are young
 const youngs = ["BN21", "BN22", "BN41", "BN42", "IT21", "IT22","IT41", "IT42", "NB21", "NB22","NB23","NB41","NB42"]
 const dario_youngs = ["RJ01", "RJ02", "RJ03", "RJ04", "RJ05", "RJ06", "RJ07", "RJ08", "RJ09", "RJ10", "RJ11", "RJ12",
-    "RJ25", "RJ26", "RJ27", "RJ28", "RJ29", "RJ30", "RJ31", "RJ32", "RJ33", "RJ34", "RJ35", "RJ36"]
+    "RJ25", "RJ26", "RJ27", "RJ28", "RJ29", "RJ30", "RJ31", "RJ32", "RJ33", "RJ34", "RJ35", "RJ36",
+    "RJ49", "RJ50", "RJ51", "RJ52", "RJ53", "RJ54", "RJ55", "RJ56", "RJ57", "RJ58", "RJ59", "RJ60",]
 
 # specifies sex
 const females = ["RJ25", "RJ26", "RJ27", "RJ28", "RJ29", "RJ30", "RJ31", "RJ32", "RJ33", "RJ34", "RJ35", "RJ36",
     "RJ37", "RJ38", "RJ39", "RJ40", "RJ41", "RJ42", "RJ43", "RJ44", "RJ45", "RJ46", "RJ47", "RJ48",
+    "RJ49", "RJ50", "RJ51", "RJ52", "RJ53", "RJ54", "RJ55", "RJ56", "RJ57", "RJ58", "RJ59", "RJ60",
+    "RJ61", "RJ62", "RJ63", "RJ64", "RJ65", "RJ66", "RJ67", "RJ68", "RJ69", "RJ70",
     "CD03", "CD04", "CD07", "CD08", "CD11", "CD12", "CD14", "CD15", "CD16", "CD20", "CD21", "CD22"]
 
 # Treatment information
