ae0480c652f4c1f23773dc9c50a5491be3304eb4
DarioSarra <dariosarra@gmail.com>
2022-10-13 08:07:23 +0100

    minor

--- src/Analysis/PostReview/6-CumRewTimeModel.jl
+++ src/Analysis/PostReview/6-CumRewTimeModel.jl
@@ -8,13 +8,13 @@ aic(Age_CR)
 aic(Age_Full)
 ##
 Age_CR_bdf = CSV.read(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Age_CumRew_Bootstrap.csv"))
-# Age_CR_bdf = bootstrapdf(Age_p,Age_CR; n = 100)
-# transform!(Age_CR_bdf,:names => ByRow(x -> replace(x,r"centered: \d{1}" => "",
-#     ")" => "", "("=> "",
-#     "Streak_zscore" => "Trial",
-#     "LogOut_zscore" => "PokeTime",
-#     "&" => "&\n")) => :Variable)
-# CSV.write(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Age_CumRew_Bootstrap.csv"),Age_CR_bdf)
+Age_CR_bdf = bootstrapdf(Age_p,Age_CR; n = 1000)
+transform!(Age_CR_bdf,:names => ByRow(x -> replace(x,r"centered: \d{1}" => "",
+    ")" => "", "("=> "",
+    "Streak_zscore" => "Trial",
+    "LogOut_zscore" => "PokeTime",
+    "&" => "&\n")) => :Variable)
+CSV.write(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Age_CumRew_1000Bootstrap.csv"),Age_CR_bdf)
 @df Age_CR_bdf scatter(:coef, :Variable, xerror = :err, legend = false, markercolor = :grey)
     vline!([0,0], color = :red, linestyle = :dash, xlabel = "Estimated coefficients (100 samp)")
 savefig(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Age_CumRew_Bootstrap.png"))
@@ -27,13 +27,13 @@ aic(Cas_CR)
 aic(Cas_Full)
 ##
 Cas_CR_bdf = CSV.read(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Cas_CumRew_Bootstrap.cs"))
-# Cas_CR_bdf = bootstrapdf(Cas_p,Cas_CR; n = 100)
-# transform!(Cas_CR_bdf,:names => ByRow(x -> replace(x,r"centered: \d{1}" => "",
-#     ")" => "", "("=> "",
-#     "Streak_zscore" => "Trial",
-#     "LogOut_zscore" => "PokeTime",
-#     "&" => "&\n")) => :Variable)
-CSV.write(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Cas_CumRew_Bootstrap.csv"),Cas_CR_bdf)
+Cas_CR_bdf = bootstrapdf(Cas_p,Cas_CR; n = 1000)
+transform!(Cas_CR_bdf,:names => ByRow(x -> replace(x,r"centered: \d{1}" => "",
+    ")" => "", "("=> "",
+    "Streak_zscore" => "Trial",
+    "LogOut_zscore" => "PokeTime",
+    "&" => "&\n")) => :Variable)
+CSV.write(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Cas_CumRew_1000Bootstrap.csv"),Cas_CR_bdf)
 @df Cas_CR_bdf scatter(:coef, :Variable, xerror = :err, legend = false, markercolor = :grey)
     vline!([0,0], color = :red, linestyle = :dash, xlabel = "Estimated coefficients (100 samp)")
 savefig(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Cas_CumRew_Bootstrap.png"))
