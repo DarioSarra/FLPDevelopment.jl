0e2836376ff2868ff05b1dfb5562847ea489fb77
DarioSarra <dariosarra@gmail.com>
2023-01-30 17:29:11 +0000

    test

--- src/Analysis/PostReview/1-Rewards.jl
+++ src/Analysis/PostReview/1-Rewards.jl
@@ -5,7 +5,7 @@ Age_Rewards = Difference(Age_s, :Age, :Num_Rewards; ylims = (0,1.15),
     Age_Rewards.plt
 Age_Rewards.test
 Age_Rewards.groupdf
-savefig(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Age_Rewards.png"))
+# savefig(joinpath(replace(path,basename(path)=>""),"Development_Figures","Review", "Age_Rewards.png"))
 
 ##
 Cas_Rewards = Difference(Cas_s, :Virus, :Num_Rewards; ylims = (0,1.23),
